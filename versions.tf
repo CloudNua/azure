84e22065144ffccd25716541a06079f193f747e0
Enda Kelly <kelly.enda@gmail.com>
2021-05-25 13:58:31 +0100

    tf template creation

---
+++ versions.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.67.0"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = ">= 3.6.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.0.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.0.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+
+  }
+}
