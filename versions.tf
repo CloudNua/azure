bd00cb5f9549240f22a0f4ccc6d95ab084ceff06
Enda Kelly <kelly.enda@gmail.com>
2021-05-25 15:09:01 +0100

    fix: versions tf file

--- versions.tf
+++ versions.tf
@@ -8,34 +8,33 @@ terraform {
     }
   }
 
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-
-    gitlab = {
-      source  = "gitlabhq/gitlab"
-      version = ">= 3.6.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "2.0.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "3.0.0"
-    }
-
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
-
+  kubernetes = {
+    source = "hashicorp/kubernetes"
   }
+
+  gitlab = {
+    source  = "gitlabhq/gitlab"
+    version = ">= 3.6.0"
+  }
+
+  random = {
+    source  = "hashicorp/random"
+    version = "3.0.0"
+  }
+
+  local = {
+    source  = "hashicorp/local"
+    version = "2.0.0"
+  }
+
+  null = {
+    source  = "hashicorp/null"
+    version = "3.0.0"
+  }
+
+  template = {
+    source  = "hashicorp/template"
+    version = "2.2.0"
+  }
+
 }
